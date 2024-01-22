cf1833aeddbe2aceee935594b716e06138e606ad
Sultan Ahmed <146020547+Faizanmedico@users.noreply.github.com>
2024-01-22 11:20:42 +0500

    Add files via upload
    بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ........My First JavaScript.....Sultan Ahmed

---
+++ myScript.js
@@ -0,0 +1,11 @@
+document.write(" بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ");
+
+let x, y, z;  
+x = 5;        
+y = 6;        
+z = x + y;   
+
+document.getElementById("demo").innerHTML =
+"The value of z is " + z + ".";
+
+document.write(" <br> بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ");
